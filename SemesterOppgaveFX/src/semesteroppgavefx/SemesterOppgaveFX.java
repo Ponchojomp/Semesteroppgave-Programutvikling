c4cc6c8926f8b7ac40e3ab28f80a5fc001fb0692
Ponchojomp <42622525+Ponchojomp@users.noreply.github.com>
2019-04-01 14:14:00 +0200

    Ny fil

--- SemesterOppgaveFX/src/semesteroppgavefx/SemesterOppgaveFX.java
+++ SemesterOppgaveFX/src/semesteroppgavefx/SemesterOppgaveFX.java
@@ -7,12 +7,11 @@
 package semesteroppgavefx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 /**
  *
  * @author leost
@@ -20,25 +19,13 @@ import javafx.stage.Stage;
 public class SemesterOppgaveFX extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
